7c7506872363a8860585ecb80c378126147a4cbe
patoagencja <daniel@patoagencja.com>
2026-02-16 15:36:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ def daily_summaries():
 
 # Scheduler - codziennie o 17:00
 scheduler = BackgroundScheduler(timezone=pytz.timezone('Europe/Warsaw'))
-scheduler.add_job(daily_summaries, 'cron', hour=17, minute=0)
+scheduler.add_job(daily_summaries, 'cron', hour=15, minute=38)
 scheduler.start()
 
 # Uruchom bota
